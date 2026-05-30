7b33fef689905c44ed28a6376f2d0ff6c3310f83
Andromedu4 <thedevines73@gmail.com>
2026-05-30 13:04:27 +0000

    Add Codewars folder with my own solutions

---
+++ code-wars/8kyu/find_max_min_value.py
@@ -0,0 +1,15 @@
+def main(arr):
+    print(maximum(arr))
+    print(minimum(arr))
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
+x = [4,6,2,1,9,63,-134,566]
+y = [-52, 56, 30, 29, -54, 0, -110]
+
+if __name__ == '__main__':
+    main(x)
