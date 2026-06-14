3581c5e7018e172c612f968d088ff701febb0721
Andromedu4 <thedevines73@gmail.com>
2026-06-14 07:40:01 +0000

    Add final solution and test

--- code-wars/8kyu/Remove_Space.py
+++ code-wars/8kyu/Remove_Space.py
@@ -8,10 +8,13 @@ Examples (Input -> Output):
 "8aaaaa dddd r     " -> "8aaaaaddddr"
 """
 
+string = "8 j 8   mBliB8g  imjB8B8  jl"
+
 def no_space(x):
     return x.replace(' ' ,'')
 
-def no_space(x):
+def no_space2(x):
     return "".join(x.split())
 
+print(no_space2(string))
 
