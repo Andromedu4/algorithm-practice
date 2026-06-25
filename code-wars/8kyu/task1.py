7f9a539262e6788a7835664f7cf00645dffdfd07
Andromedu4 <thedevines73@gmail.com>
2026-06-25 06:10:39 +0000

    Refactor

--- code-wars/8kyu/task1.py
+++ code-wars/8kyu/task1.py
@@ -5,8 +5,6 @@ But you're an ambitious person. You want to know if you're better than the avera
 
 You receive an array with your peers' test scores. Now calculate the average and compare your score!
 
-Return true if you're better, else false!
-
 Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
 """
 def better_than_average(class_points, your_points):
