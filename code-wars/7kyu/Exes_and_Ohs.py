53252a47ac9feba6a103a35fc72bfe5dfbabf465
Andromedu4 <thedevines73@gmail.com>
2026-06-29 13:51:41 +0000

    Refactor

--- code-wars/7kyu/Exes_and_Ohs.py
+++ code-wars/7kyu/Exes_and_Ohs.py
@@ -3,8 +3,6 @@ Check to see if a string has the same amount of 'x's and 'o's. The method must r
 
 Examples input/output:
 
-XO("ooxx") => true
-XO("xooxx") => false
 XO("ooxXm") => true
 XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 """
