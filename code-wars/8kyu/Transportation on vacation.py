a4344cf87f3956ad958b5784fb5466fe6f5777f6
Andromedu4 <thedevines73@gmail.com>
2026-07-05 12:59:52 +0000

    Solution 1

--- code-wars/8kyu/Transportation on vacation.py
+++ code-wars/8kyu/Transportation on vacation.py
@@ -7,3 +7,11 @@ Every day you rent the car costs $40. If you rent the car for 7 or more days, yo
 
 Write a code that gives out the total amount for different days(d).
 """
+
+def rental_car_cost(d):
+    result = d * 40
+    if d >= 7:
+        result -= 50
+    elif d >= 3:
+        result -= 20
+    return result
