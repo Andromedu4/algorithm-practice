cc3658bcb92f6762994e4b8d3c0dda6d0fdec62b
Andromedu4 <thedevines73@gmail.com>
2026-07-07 19:50:10 +0000

    Refactor

--- code-wars/8kyu/Avg_Number.py
+++ code-wars/8kyu/Avg_Number.py
@@ -2,15 +2,7 @@
 Write a function which calculates the average of the numbers in a given array.
 
 Note: Empty arrays should return 0.
-"""
-
-''' 
-Solution 1
-    array = [1,5,3]
-
-    def find_average(array):
-
-'''
+"
 
 array = [1,5,3]
 
