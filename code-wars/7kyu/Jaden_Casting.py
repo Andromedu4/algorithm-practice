7231cf153be10c355fe7f928d284b07f432b3ccc
Andromedu4 <thedevines73@gmail.com>
2026-07-10 04:36:08 +0000

    Add Solution 2

--- code-wars/7kyu/Jaden_Casting.py
+++ code-wars/7kyu/Jaden_Casting.py
@@ -1,4 +1,4 @@
-
+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
 """
 Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
@@ -9,6 +9,9 @@ Example:
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 """
+import string
+toJadenCase = string.capwords
 
 def to_jaden_case(string):
     return ' '.join(word.capitalize() for word in string.split())
+
