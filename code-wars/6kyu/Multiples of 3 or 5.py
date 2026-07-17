65b5cfd6f76a0cdbbfb0265276fd0775392b6735
Andromedu4 <thedevines73@gmail.com>
2026-07-17 08:54:46 +0000

    Solution 2

--- code-wars/6kyu/Multiples of 3 or 5.py
+++ code-wars/6kyu/Multiples of 3 or 5.py
@@ -12,3 +12,10 @@ Courtesy of projecteuler.net (Problem 1)
 
 def solution(number):
     return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
+
+def solution2(number):
+    sum = 0
+    for i in range(number):
+        if (i % 3) == 0 or (i % 5) == 0:
+            sum += i
+    return sum
