39cdf9a888188beffa84dfc90c8df131777c35f8
Andromedu4 <thedevines73@gmail.com>
2026-07-20 11:12:57 +0000

    Refactor

--- code-wars/6kyu/Multiples of 3 or 5.py
+++ code-wars/6kyu/Multiples of 3 or 5.py
@@ -6,8 +6,6 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 be
 Additionally, if the number is negative, return 0.
 
 Note: If a number is a multiple of both 3 and 5, only count it once.
-
-Courtesy of projecteuler.net (Problem 1)
 """
 
 #Solution 1
