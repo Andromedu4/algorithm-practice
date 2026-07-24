5b7477322fcfffe482b5f38b7b33493eb29773d7
Andromedu4 <thedevines73@gmail.com>
2026-07-24 16:22:44 +0000

    VOT ETO COMMIT

--- code-wars/6kyu/Multiples of 3 or 5.py
+++ code-wars/6kyu/Multiples of 3 or 5.py
@@ -3,7 +3,6 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
-Additionally, if the number is negative, return 0.
 
 Note: If a number is a multiple of both 3 and 5, only count it once.
 """
